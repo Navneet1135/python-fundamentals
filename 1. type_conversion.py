0bf82b92e90fa12d0ae801b909af453b2c917a7d
Navneet1135 <navneetpal1135@gmail.com>
2026-03-21 14:11:22 +0530

    Add type conversion examples in Python
    Demonstrates type conversion in Python with examples.

---
+++ 1. type_conversion.py
@@ -0,0 +1,16 @@
+# Type Conversion in Python
+
+# Boolean to String
+not_valid = False
+x = str(not_valid)
+print(x)  # Output: False
+
+# String to Integer
+str_value = "27"
+int_27 = int(str_value)
+print(int_27)  # Output: 27
+
+# Integer to Float
+a = 27
+num = float(a)
+print(num)  # Output: 27.0
